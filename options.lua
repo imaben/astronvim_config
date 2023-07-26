4fd776e8c9a215628e100d78f2f490f1b84b5c5f
maben <www.maben@foxmail.com>
2023-07-26 16:28:48 +0800

    change shiftwidth

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    shiftwidth = 4, -- Number of space inserted for indentation
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
